fe3783ffe07496016448aa936f04e391c6d74460
Anna Sedlackova <axs1202@case.edu>
2019-10-03 13:54:22 -0400

    adding number of possible solutions algo

--- hw2.py
+++ hw2.py
@@ -3,20 +3,21 @@ import numpy as np
 # constants to calculate scores
 gap_penalty = -1  # indel
 match_award = 1  # match
-mismatch_penalty = -1  # mismatch
+mismatch_penalty = -1  # mismatch # TODO rewrite this
 
-#TODO check if the inputs match the signs expected
+# TODO check if the inputs match the signs expected
+
 
 class Align:
 
     def __init__(self, alignment, match, mismatch, indel, string_one, string_two):
-        self.alignment = alignment
+        self.alignment = 'l'
+        self.path = 0
         self.match = match
         self.mismatch = mismatch
         self.indel = indel
         self.string_one = string_one.upper()
         self.string_two = string_two.upper()
-        self.score_matrix = np.zeros((len(string_one), len(string_two)))
 
     def align_sequences(self):
         # Store length of two sequences in temp variables
@@ -26,28 +27,33 @@ class Align:
         # Generate matrix of zeros to store scores at each iteration
         score_matrix = np.zeros((len(self.string_two) + 1, len(self.string_one) + 1), np.int)
 
-    # Local alignemnt algo
-    def smith_waterman(self, n, m, score_matrix):
-        # Fill out all other values in the score matrix
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                # Calculate the max score by checking the top, left, and diagonals
-                match = score_matrix[i - 1][j - 1] + self.compute_score(self.string_one[j - 1], self.string_two[i - 1])
-                delete = score_matrix[i - 1][j] + gap_penalty
-                insert = score_matrix[i][j - 1] + gap_penalty
+        if self.alignment == 'l':
+            return self.needleman_wunsch(n, m, score_matrix)
+        else:
+            return 0
 
-                # Record the maximum score from the three scenarios
-                score_matrix[i][j] = max(match, delete, insert)
-
-        print(score_matrix)
-
-        b, optimal_score = self.backtrack_sw(score_matrix)
-
-    def backtrack_sw(self, score_matrix):
+    # # Local alignemnt algo
+    # def smith_waterman(self, n, m, score_matrix):
+    #     # Fill out all other values in the score matrix
+    #     for i in range(1, m + 1):
+    #         for j in range(1, n + 1):
+    #             # Calculate the max score by checking the top, left, and diagonals
+    #             match = score_matrix[i - 1][j - 1] + self.compute_score(self.string_one[j - 1], self.string_two[i - 1])
+    #             delete = score_matrix[i - 1][j] + gap_penalty
+    #             insert = score_matrix[i][j - 1] + gap_penalty
+    #
+    #             # Record the maximum score from the three scenarios
+    #             score_matrix[i][j] = max(match, delete, insert)
+    #
+    #     print(score_matrix)
+    #
+    #     b, optimal_score = self.backtrack_sw(score_matrix)
+    #
+    # def backtrack_sw(self, score_matrix):
 
 
     # Global alignment algo
-    def needleman_wunsch(self, n , m, score_matrix):
+    def needleman_wunsch(self, n, m, score_matrix):
 
         # # Store length of two sequences in temp variables
         # n = len(self.string_one)
@@ -77,13 +83,36 @@ class Align:
                 score_matrix[i][j] = max(match, delete, insert)
 
         print(score_matrix)
-
         return self.backtrack_nw(score_matrix)
+
+    def num_optimal_solution(self, i, j, score_matrix, path):
+        if i == 1 and j == 1:
+            return path + 1
+
+        elif i == 1:
+            return self.num_optimal_solution(i, j - 1, score_matrix, path)
+
+        elif j == 1:
+            return self.num_optimal_solution(i - 1, j, score_matrix, path)
+
+        else:
+            if (self.string_one(i) == self.string_two(i)) and (score_matrix[i][j] == score_matrix[i - 1][j - 1] + match_award):
+                return self.num_optimal_solution(i - 1, j - 1, score_matrix, path)
+
+            if (self.string_one(i) != self.string_two(i)) and (score_matrix[i][j] == score_matrix[i - 1][j - 1] + mismatch_penalty):
+                return self.num_optimal_solution(i - 1, j - 1, score_matrix, path)
+
+            if score_matrix[i][j] == score_matrix[i][j-1] + gap_penalty:
+                return self.num_optimal_solution(i, j-1, score_matrix, path)
+
+            if score_matrix[i][j] == score_matrix[i-1][j] + gap_penalty:
+                return self.num_optimal_solution(i-1, j, score_matrix, path)
+
+            return 0
 
     def backtrack_nw(self, score_matrix):
 
         # Traceback and compute the alignment
-
         # Create variables to store alignment
         alignment_one = ""
         alignment_two = ""
@@ -102,15 +131,18 @@ class Align:
 
             # Check to figure out which cell the current score was calculated from,
             # then update i and j to correspond to that cell.
+
             if score_current == score_diagonal + self.compute_score(self.string_one[j - 1], self.string_two[i - 1]):
                 alignment_one += self.string_one[j - 1]
                 alignment_two += self.string_two[i - 1]
                 i -= 1
                 j -= 1
+
             elif score_current == score_up + gap_penalty:
-                alignment_one += self.string_two[j - 1]
+                alignment_one += self.string_one[j - 1]
                 alignment_two += '-'
                 j -= 1
+
             elif score_current == score_left + gap_penalty:
                 alignment_one += '-'
                 alignment_two += self.string_two[i - 1]
@@ -153,9 +185,8 @@ def main():
     print("Results:")
 
     #align = Align('g', -1, -1, -1, 'ATTACA', 'ATGCT')
-    #TODO breaks with free and fire
-    align = Align('g', -1, -1, -1, 'CATCAT', 'CAT')
-    output1, output2, optimal_score = align.needleman_wunsch()
+    align = Align('g', -1, -1, -1, 'catcat', 'cat')
+    output1, output2, optimal_score = align.align_sequences()
 
     print('Optimal Score:', optimal_score)
     print('Num Optimal Solutions:')
