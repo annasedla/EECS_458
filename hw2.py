a8ce18f6fe8e630ee4ab9efe27b6a4ac0f5e77e1
Anna Sedlackova <axs1202@case.edu>
2019-10-06 20:26:54 -0400

    both algo should be working now

--- hw2.py
+++ hw2.py
@@ -6,16 +6,26 @@ import numpy as np
 
 class Align:
 
-    def __init__(self, alignment, match, mismatch, indel, string_one, string_two):
+    def __init__(self):
+        self.alignment = ''
+        self.path = 0
+        self.match = 1
+        self.mismatch = -1
+        self.indel = -1
+        self.string_one = ''
+        self.string_two = ''
+
+    def align_sequences(self, alignment, string_one, string_two, path=0, match=1, mismatch=-1, indel=-1):
+
+        # Set all the global variables
         self.alignment = alignment
-        self.path = 0  # TODO remember to reset path
+        self.string_one = string_one
+        self.string_two = string_two
+        self.path = path
         self.match = match
         self.mismatch = mismatch
         self.indel = indel
-        self.string_one = string_one.upper()
-        self.string_two = string_two.upper()
 
-    def align_sequences(self):
         # Store length of two sequences in temp variables
         n = len(self.string_one)
         m = len(self.string_two)
@@ -53,10 +63,19 @@ class Align:
                 # Record the maximum score from the three scenarios
                 score_matrix[i][j] = max(match, delete, insert)
 
+        """
+        OUTPUT
+        """
+        print("RESULTS:")
+        print()
+        print('~~Global alignment~~')
+        print("Score Matrix:")
         print(score_matrix)
+
         self.num_optimal_solution_nw(len(self.string_two), len(self.string_one), score_matrix)
-        print('Num optimal', self.path)
-        return self.backtrack(score_matrix)
+
+        print('Number of optimal solutions:', self.path)
+        self.backtrack(score_matrix)
 
     # Local alignemnt algo
     def smith_waterman(self, n, m, score_matrix):
@@ -72,18 +91,24 @@ class Align:
                 # Record the maximum score from the three scenarios
                 score_matrix[i][j] = max(match, delete, insert)
 
-        print(score_matrix)
-
         num_indices = np.where(score_matrix == score_matrix.max())
 
         for i in range(0, len(num_indices[0])):
             self.num_optimal_solution_sw(num_indices[0][i], num_indices[1][i], score_matrix)
 
-            print('Num optimal', self.path)
+        """
+        OUTPUT
+        """
+        print("RESULTS:")
+        print()
+        print('~~Local alignment~~')
+        print("Score Matrix:")
+        print(score_matrix)
 
-        print('Final num optimal', self.path)
+        self.num_optimal_solution_sw(len(self.string_two), len(self.string_one), score_matrix)
 
-        return self.backtrack(score_matrix)
+        print('Number of optimal solutions:', self.path)
+        self.backtrack(score_matrix)
 
     def num_optimal_solution_nw(self, i, j, score_matrix):
         if i == 0 and j == 0:
@@ -126,60 +151,6 @@ class Align:
 
             if score_matrix[i][j] == score_matrix[i - 1][j] + self.indel:
                 self.num_optimal_solution_sw(i - 1, j, score_matrix)
-
-    # def backtrack_nw(self, score_matrix):
-    #
-    #     # Traceback and compute the alignment
-    #     # Create variables to store alignment
-    #     alignment_one = ""
-    #     alignment_two = ""
-    #
-    #     # Start from the bottom right cell in matrix
-    #     i = (len(self.string_two))
-    #     j = len(self.string_one)
-    #
-    #     optimal_score = score_matrix[i][j]
-    #
-    #     while i > 0 and j > 0:
-    #         score_current = score_matrix[i][j]
-    #         score_diagonal = score_matrix[i - 1][j - 1]
-    #         score_up = score_matrix[i][j - 1]
-    #         score_left = score_matrix[i - 1][j]
-    #
-    #         # Check to figure out which cell the current score was calculated from,
-    #         # then update i and j to correspond to that cell.
-    #
-    #         if score_current == score_diagonal + self.compute_score(self.string_one[j - 1], self.string_two[i - 1]):
-    #             alignment_one += self.string_one[j - 1]
-    #             alignment_two += self.string_two[i - 1]
-    #             i -= 1
-    #             j -= 1
-    #
-    #         elif score_current == score_up + self.indel:
-    #             alignment_one += self.string_one[j - 1]
-    #             alignment_two += '-'
-    #             j -= 1
-    #
-    #         elif score_current == score_left + self.indel:
-    #             alignment_one += '-'
-    #             alignment_two += self.string_two[i - 1]
-    #             i -= 1
-    #
-    #     # Finish tracing up to the top left cell
-    #     while j > 0:
-    #         alignment_one += self.string_one[j - 1]
-    #         alignment_two += '-'
-    #         j -= 1
-    #     while i > 0:
-    #         alignment_one += '-'
-    #         alignment_two += self.string_two[i - 1]
-    #         i -= 1
-    #
-    #     # reverse the sequences
-    #     alignment_one = alignment_one[::-1]
-    #     alignment_two = alignment_two[::-1]
-    #
-    #     return alignment_one, alignment_two, optimal_score
 
     def backtrack(self, score_matrix):
 
@@ -228,10 +199,6 @@ class Align:
                 alignment_two += self.string_two[i - 1]
                 i -= 1
 
-        # reverse the sequences
-        alignment_one = alignment_one[::-1]
-        alignment_two = alignment_two[::-1]
-
         if self.alignment == 'g':
             # Finish tracing up to the top left cell
             while j > 0:
@@ -243,9 +210,13 @@ class Align:
                 alignment_two += self.string_two[i - 1]
                 i -= 1
 
-        print(alignment_one)
-        print(alignment_two)
-        print(optimal_score)
+        # reverse the sequences
+        alignment_one = alignment_one[::-1]
+        alignment_two = alignment_two[::-1]
+
+        print('Alignment one:', alignment_one)
+        print('Alignment two:', alignment_two)
+        print('Optimal score:', optimal_score)
 
     # A function for determining the score between any two bases in alignment
     def compute_score(self, a, b):
@@ -258,22 +229,8 @@ class Align:
 
 
 def main():
-    """
-    Output optimal score
-    number of solutions that achieve optimal score
-    actual alignment
-    """
-
-    print("Results:")
-
-    #align = Align('g', 1, -1, -1, 'ATTACA', 'ATGCT')
-    align = Align('l', 1, -1, -1, 'catcat', 'cat')
-    # output1, output2, optimal_score = align.align_sequences()
-    output1, output2, optimal_score = align.align_sequences()
-
-    print('Optimal Score:', optimal_score)
-    print('Num Optimal Solutions:')
-    print('Actual Alignments:', "\n" + output1 + "\n" + output2)
+    align = Align()
+    align.align_sequences('l', 'catcat', 'cat', 0, 1, -1, -1)
 
 
 if __name__ == "__main__":
