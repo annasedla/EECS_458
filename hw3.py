fee1b382e2383d963584939aa3dc5e1bdd833950
Anna Sedlackova <axs1202@case.edu>
2019-10-29 15:42:40 -0400

    adding initial files for viterbi

---
+++ hw3.py
@@ -0,0 +1,18 @@
+#matrix, backward, forward, posterior, main
+
+
+class Viterbi:
+
+    def __init__(self, observations, states, initial_probabilities, transition_probabilities, emission_probabilities):
+        self.observations = observations
+        self.states = states
+        self.initial_probabilities = initial_probabilities
+        self.transition_probabilities= transition_probabilities
+        self.emission_probabilities = emission_probabilities
+
+def main():
+    print('sup')
+
+
+if __name__ == "__main__":
+    main()
